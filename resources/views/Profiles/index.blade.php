827e9801473db23154236e5e13e3ec2a74cdf7cf
Yousif AL-Qadi <yosefu404@gmail.com>
2020-07-15 10:40:23 +0900

    Editing the profile is working now.

--- resources/views/Profiles/index.blade.php
+++ resources/views/Profiles/index.blade.php
@@ -4,20 +4,25 @@
 <div class="container">
     <div class="row">
         <div class="col-3 p-5">
-            <img src="https://picsum.photos/536/354" class="rounded-circle" style="width:100%"></div>
+            <img src="/storage/{{ $user->profile->image }}" class="rounded-circle w-100" style="width:100%"></div>
 
         <div class="col-9 pt-5">
             <div class="d-flex justify-content-between align-items-baseline">
                 <h1>{{ $user->username }}</h1>
+                @can('update',$user->profile)
                 <a href="/p/create">Add New Post</a>
+                @endcan
             </div>
+            @can('update',$user->profile)
+            <a href="/profile/{{ $user->id }}/edit">Edit Profile</a>
+            @endcan
             <div class="d-flex">
                 <div class="pr-5"><strong>{{ $user->posts->count()}}</strong> posts</div>
                 <div class="pr-5"><strong>12k</strong> followers</div>
                 <div class="pr-5"><strong>222</strong> following</div>
             </div>
-            <div class="pt-4 font-weight-bold">title</div>
-            <div>dis</div>
+            <div class="pt-4 font-weight-bold">{{ $user->profile->title }}</div>
+            <div>{{ $user->profile->description }}</div>
             <div> <a href="#">{{ $user->profile->url ?? 'N/A'}}</a></div>
         </div>
     </div>
